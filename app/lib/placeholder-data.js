cc291f6a96fb9b84e1c01970d92b62d13e37c2ae
Galymzhan Muratov <galimzhanmuratv@gmail.com>
2024-03-28 22:40:25 +0500

    tutorial complete

--- app/lib/placeholder-data.js
+++ app/lib/placeholder-data.js
@@ -1,3 +1,4 @@
+import { sql } from '@vercel/postgres';
 // This file contains placeholder data that you'll be replacing with real data in the Data Fetching chapter:
 // https://nextjs.org/learn/dashboard-app/fetching-data
 const users = [
